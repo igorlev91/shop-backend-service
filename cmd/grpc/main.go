c91850a5bb9d32e407ff6c733990185858fc407e
IgorLev <igorlevdansky91@gmail.com>
2026-01-08 18:03:55 +0300

    create upload image product feature

---
+++ cmd/grpc/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	github.com/igorlev91/golang-grpc-ecommerce/internal/grpcmiddleware"
+	github.com/igorlev91/golang-grpc-ecommerce/internal/handler"
+	github.com/igorlev91/golang-grpc-ecommerce/internal/repository"
+	github.com/igorlev91/golang-grpc-ecommerce/internal/service"
+	github.com/igorlev91/golang-grpc-ecommerce/pb/auth"
+	github.com/igorlev91/golang-grpc-ecommerce/pb/product"
+	github.com/igorlev91/golang-grpc-ecommerce/pkg/database"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	gocache "github.com/patrickmn/go-cache"
+)
+
+func main() {
+	ctx := context.Background()
+	godotenv.Load()
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Panicf("Error when listening %v", err)
+	}
+
+	db := database.ConnectDB(ctx, os.Getenv("DB_URI"))
+	log.Println("Connected to database")
+
+	cacheService := gocache.New(time.Hour*24, time.Hour)
+
+	authMiddleware := grpcmiddleware.NewAuthMiddleware(cacheService)
+
+	authRepository := repository.NewAuthRepository(db)
+	authService := service.NewAuthService(authRepository, cacheService)
+	authHandler := handler.NewAuthHandler(authService)
+
+	productRepository := repository.NewProductRepository(db)
+	productService := service.NewProductService(productRepository)
+	productHandler := handler.NewProductHandler(productService)
+
+	server := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			grpcmiddleware.ErrorMiddleware,
+			authMiddleware.Middleware,
+		),
+	)
+
+	auth.RegisterAuthServiceServer(server, authHandler)
+	product.RegisterProductServiceServer(server, productHandler)
+
+	if os.Getenv("ENVIRONTMENT") == "dev" {
+		reflection.Register(server)
+		log.Println("Reflection is registered.")
+	}
+
+	log.Println("Server start in port :50051")
+	if err := server.Serve(lis); err != nil {
+		log.Panicf("Server error %v", err)
+	}
+}
